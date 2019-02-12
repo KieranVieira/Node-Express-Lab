8f4c0b2ceae879a941897d688f219ac2e572a532
KieranVieira <kieranvieira@live.com>
2019-02-12 15:58:25 -0500

    completed .get for id

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -22,7 +22,6 @@ router.post('/', (req, res) => {
         console.log(error)
         res.status(500).json({ error: "There was an error while saving the post to the database" })
     }
-    
 });
 
 router.get('/', (req, res) => {
@@ -34,6 +33,23 @@ router.get('/', (req, res) => {
             console.log(err)
             res.status(500).json({ error: "The posts information could not be retrieved." })
         })
+});
+
+router.get('/:id', (req, res) => {
+    const postId = req.params.id;
+    db 
+    .findById(postId)
+    .then(post => {
+        if(post.length > 0){
+            res.status(200).json(post)
+        }else{
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    })  
 });
 
 module.exports = router;
